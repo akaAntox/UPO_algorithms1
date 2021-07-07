d05fb974fca6ff653dd06c5ad1d8141cf3aedd7b
Anton Iliev <akaantoninox@gmail.com>
2021-07-07 15:52:52 +0200

    Ex1 hashtable solved

--- src/hashtable.c
+++ src/hashtable.c
@@ -19,7 +19,6 @@
  * along with UPOalglib.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-
 #include <assert.h>
 #include "hashtable_private.h"
 #include <math.h>
@@ -28,9 +27,7 @@
 #include <upo/error.h>
 #include <upo/utility.h>
 
-
 /*** EXERCISE #1 - BEGIN of HASH TABLE with SEPARATE CHAINING ***/
-
 
 upo_ht_sepchain_t upo_ht_sepchain_create(size_t m, upo_ht_hasher_t key_hash, upo_ht_comparator_t key_cmp)
 {
@@ -38,8 +35,8 @@ upo_ht_sepchain_t upo_ht_sepchain_create(size_t m, upo_ht_hasher_t key_hash, upo
     size_t i = 0;
 
     /* preconditions */
-    assert( key_hash != NULL );
-    assert( key_cmp != NULL );
+    assert(key_hash != NULL);
+    assert(key_cmp != NULL);
 
     /* Allocate memory for the hash table type */
     ht = malloc(sizeof(struct upo_ht_sepchain_s));
@@ -52,7 +49,7 @@ upo_ht_sepchain_t upo_ht_sepchain_create(size_t m, upo_ht_hasher_t key_hash, upo
     if (m > 0)
     {
         /* Allocate memory for the array of slots */
-        ht->slots = malloc(m*sizeof(upo_ht_sepchain_slot_t));
+        ht->slots = malloc(m * sizeof(upo_ht_sepchain_slot_t));
         if (ht->slots == NULL)
         {
             perror("Unable to allocate memory for slots of the Hash Table with Separate Chaining");
@@ -121,56 +118,122 @@ void upo_ht_sepchain_clear(upo_ht_sepchain_t ht, int destroy_data)
     }
 }
 
-void* upo_ht_sepchain_put(upo_ht_sepchain_t ht, void *key, void *value)
+void *upo_ht_sepchain_put(upo_ht_sepchain_t ht, void *key, void *value)
 {
+    if (ht == NULL)
+        return;
+
     void *old_value = NULL;
 
-    /* TO STUDENTS:
-     *  Remove the following two lines and put here your implementation. */
-    fprintf(stderr, "To be implemented!\n");
-    abort();
+    size_t h = ht->key_hash(key, sizeof(key));
+    upo_ht_sepchain_list_node_t *n = ht->slots[h].head;
+
+    while (n != NULL && key != n->key)
+        n = n->next;
+    if (n == NULL)
+    {
+        n = malloc(sizeof(upo_ht_sepchain_list_node_t));
+        n->key = key;
+        n->value = value;
+        n->next = ht->slots[h].head;
+        ht->slots[h].head = n;
+        ht->size++;
+    }
+    else
+    {
+        old_value = n->value;
+        n->value = value;
+    }
 
     return old_value;
 }
 
 void upo_ht_sepchain_insert(upo_ht_sepchain_t ht, void *key, void *value)
 {
-    /* TO STUDENTS:
-     *  Remove the following two lines and put here your implementation. */
-    fprintf(stderr, "To be implemented!\n");
-    abort();
+    if (ht == NULL)
+        return;
+
+    size_t h = ht->key_hash(key, sizeof(key));
+    upo_ht_sepchain_list_node_t *n = ht->slots[h].head;
+
+    while (n != NULL && key != n->key)
+        n = n->next;
+    if (n == NULL)
+    {
+        n = malloc(sizeof(upo_ht_sepchain_list_node_t));
+        n->key = key;
+        n->value = value;
+        n->next = ht->slots[h].head;
+        ht->slots[h].head;
+        ht->size++;
+    }
 }
 
-void* upo_ht_sepchain_get(const upo_ht_sepchain_t ht, const void *key)
+void *upo_ht_sepchain_get(const upo_ht_sepchain_t ht, const void *key)
 {
-    /* TO STUDENTS:
-     *  Remove the following two lines and put here your implementation. */
-    fprintf(stderr, "To be implemented!\n");
-    abort();
+    if (ht == NULL)
+        return NULL;
+
+    size_t h = ht->key_hash(key, sizeof(key));
+    upo_ht_sepchain_list_node_t *n = ht->slots[h].head;
+
+    while (n != NULL && key != n->key)
+        n = n->next;
+    if (n != NULL)
+        return n->value;
+    return NULL;
 }
 
 int upo_ht_sepchain_contains(const upo_ht_sepchain_t ht, const void *key)
 {
-    /* TO STUDENTS:
-     *  Remove the following two lines and put here your implementation. */
-    fprintf(stderr, "To be implemented!\n");
-    abort();
+    if (ht == NULL)
+        return 0;
+
+    size_t h = ht->key_hash(key, sizeof(key));
+    upo_ht_sepchain_list_node_t *n = ht->slots[h].head;
+
+    while (n != NULL && key != n->key)
+        n = n->next;
+    if (n != NULL)
+        return 1;
+    return 0;
 }
 
 void upo_ht_sepchain_delete(upo_ht_sepchain_t ht, const void *key, int destroy_data)
 {
-    /* TO STUDENTS:
-     *  Remove the following two lines and put here your implementation. */
-    fprintf(stderr, "To be implemented!\n");
-    abort();
+    if (ht == NULL)
+        return;
+
+    size_t h = ht->key_hash(key, sizeof(key));
+    upo_ht_sepchain_list_node_t *n = ht->slots[h].head;
+    upo_ht_sepchain_list_node_t *p = NULL;
+
+    while (n != NULL && key != n->key)
+    {
+        p = n;
+        n = n->next;
+    }
+    if (n != NULL)
+        if (p == NULL)
+            ht->slots[h].head = n->next;
+        else
+            p->next = n->next;
+
+    ht->size--;
+    if (destroy_data)
+    {
+        free(n->key);
+        free(n->value);
+    }
+    free(n);
 }
 
 size_t upo_ht_sepchain_size(const upo_ht_sepchain_t ht)
 {
-    /* TO STUDENTS:
-     *  Remove the following two lines and put here your implementation. */
-    fprintf(stderr, "To be implemented!\n");
-    abort();
+    if (ht == NULL)
+        return 0;
+
+    return ht->size;
 }
 
 int upo_ht_sepchain_is_empty(const upo_ht_sepchain_t ht)
@@ -185,7 +248,7 @@ size_t upo_ht_sepchain_capacity(const upo_ht_sepchain_t ht)
 
 double upo_ht_sepchain_load_factor(const upo_ht_sepchain_t ht)
 {
-    return upo_ht_sepchain_size(ht) / (double) upo_ht_sepchain_capacity(ht);
+    return upo_ht_sepchain_size(ht) / (double)upo_ht_sepchain_capacity(ht);
 }
 
 upo_ht_comparator_t upo_ht_sepchain_get_comparator(const upo_ht_sepchain_t ht)
@@ -198,12 +261,9 @@ upo_ht_hasher_t upo_ht_sepchain_get_hasher(const upo_ht_sepchain_t ht)
     return ht->key_hash;
 }
 
-
 /*** EXERCISE #1 - END of HASH TABLE with SEPARATE CHAINING ***/
 
-
 /*** EXERCISE #2 - BEGIN of HASH TABLE with LINEAR PROBING ***/
-
 
 upo_ht_linprob_t upo_ht_linprob_create(size_t m, upo_ht_hasher_t key_hash, upo_ht_comparator_t key_cmp)
 {
@@ -211,8 +271,8 @@ upo_ht_linprob_t upo_ht_linprob_create(size_t m, upo_ht_hasher_t key_hash, upo_h
     size_t i = 0;
 
     /* preconditions */
-    assert( key_hash != NULL );
-    assert( key_cmp != NULL );
+    assert(key_hash != NULL);
+    assert(key_cmp != NULL);
 
     /* Allocate memory for the hash table type */
     ht = malloc(sizeof(struct upo_ht_linprob_s));
@@ -225,7 +285,7 @@ upo_ht_linprob_t upo_ht_linprob_create(size_t m, upo_ht_hasher_t key_hash, upo_h
     /* Allocate memory for the array of slots */
     if (m > 0)
     {
-        ht->slots = malloc(m*sizeof(upo_ht_linprob_slot_t));
+        ht->slots = malloc(m * sizeof(upo_ht_linprob_slot_t));
         if (ht->slots == NULL)
         {
             perror("Unable to allocate memory for slots of the Hash Table with Separate Chaining");
@@ -289,7 +349,7 @@ void upo_ht_linprob_clear(upo_ht_linprob_t ht, int destroy_data)
     }
 }
 
-void* upo_ht_linprob_put(upo_ht_linprob_t ht, void *key, void *value)
+void *upo_ht_linprob_put(upo_ht_linprob_t ht, void *key, void *value)
 {
     void *old_value = NULL;
 
@@ -309,7 +369,7 @@ void upo_ht_linprob_insert(upo_ht_linprob_t ht, void *key, void *value)
     abort();
 }
 
-void* upo_ht_linprob_get(const upo_ht_linprob_t ht, const void *key)
+void *upo_ht_linprob_get(const upo_ht_linprob_t ht, const void *key)
 {
     /* TO STUDENTS:
      *  Remove the following two lines and put here your implementation. */
@@ -353,13 +413,13 @@ size_t upo_ht_linprob_capacity(const upo_ht_linprob_t ht)
 
 double upo_ht_linprob_load_factor(const upo_ht_linprob_t ht)
 {
-    return upo_ht_linprob_size(ht) / (double) upo_ht_linprob_capacity(ht);
+    return upo_ht_linprob_size(ht) / (double)upo_ht_linprob_capacity(ht);
 }
 
 void upo_ht_linprob_resize(upo_ht_linprob_t ht, size_t n)
 {
     /* preconditions */
-    assert( n > 0 );
+    assert(n > 0);
 
     if (ht != NULL)
     {
@@ -405,12 +465,9 @@ void upo_ht_linprob_resize(upo_ht_linprob_t ht, size_t n)
     }
 }
 
-
 /*** EXERCISE #2 - END of HASH TABLE with LINEAR PROBING ***/
 
-
 /*** EXERCISE #3 - BEGIN of HASH TABLE - EXTRA OPERATIONS ***/
-
 
 upo_ht_key_list_t upo_ht_sepchain_keys(const upo_ht_sepchain_t ht)
 {
@@ -444,54 +501,51 @@ void upo_ht_linprob_traverse(const upo_ht_linprob_t ht, upo_ht_visitor_t visit, 
     abort();
 }
 
-
 /*** EXERCISE #3 - END of HASH TABLE - EXTRA OPERATIONS ***/
 
-
 /*** BEGIN of HASH FUNCTIONS ***/
-
 
 size_t upo_ht_hash_int_div(const void *x, size_t m)
 {
     /* preconditions */
-    assert( x != NULL );
-    assert( m > 0 );
+    assert(x != NULL);
+    assert(m > 0);
 
-    return *((int*) x) % m;
+    return *((int *)x) % m;
 }
 
 size_t upo_ht_hash_int_mult(const void *x, double a, size_t m)
 {
     /* preconditions */
-    assert( x != NULL );
-    assert( a > 0 && a < 1 );
-    assert( m > 0 );
+    assert(x != NULL);
+    assert(a > 0 && a < 1);
+    assert(m > 0);
 
-    return floor( m * fmod(a * *((int*) x), 1.0) );
+    return floor(m * fmod(a * *((int *)x), 1.0));
 }
 
 size_t upo_ht_hash_int_mult_knuth(const void *x, size_t m)
 {
-    return upo_ht_hash_int_mult(x, 0.5*(sqrt(5)-1), m);
+    return upo_ht_hash_int_mult(x, 0.5 * (sqrt(5) - 1), m);
 }
 
 size_t upo_ht_hash_str(const void *x, size_t h0, size_t a, size_t m)
 {
     const char *s = NULL;
-    size_t h = h0; 
+    size_t h = h0;
 
     /* preconditions */
-    assert( x != NULL );
-    assert( m > 0 );
-/*
+    assert(x != NULL);
+    assert(m > 0);
+    /*
     assert( a < m );
 */
-    assert( h0 < m );
+    assert(h0 < m);
 
-    s = *((const char**) x);
+    s = *((const char **)x);
     for (; *s; ++s)
     {
-        h = (a*h + *s) % m;
+        h = (a * h + *s) % m;
     }
 
     return h;
@@ -505,16 +559,16 @@ size_t upo_ht_hash_str_djb2(const void *x, size_t m)
 size_t upo_ht_hash_str_djb2a(const void *x, size_t m)
 {
     const char *s = NULL;
-    size_t h = 5381U; 
+    size_t h = 5381U;
 
     /* preconditions */
-    assert( x != NULL );
-    assert( m > 0 );
+    assert(x != NULL);
+    assert(m > 0);
 
-    s = *((const char**) x);
+    s = *((const char **)x);
     for (; *s; ++s)
     {
-        h = (33U*h ^ *s) % m;
+        h = (33U * h ^ *s) % m;
     }
 
     return h;
